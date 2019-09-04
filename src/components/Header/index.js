006845f79d26e76be1e445018ce521d400759240
VITOR MONTEIRO (CRGL-THIRDPARTY.COM) <vitor.christoval@samcorp.com.br>
2019-09-04 14:41:57 -0300

    clone

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from './styles';
+
+export default function Header(){
+    return(
+       <Container>
+           <h2>Pipefy - Clone</h2>
+       </Container>
+    );
+}
